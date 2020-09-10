dfd540e2e4adb5c48ff916a3a480cc3b6783a8c7
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-09-10 19:21:53 +0700

    [Update] Check format  transfer ref 🌻

--- CheckTransferRef/checkTransferRef/app.py
+++ CheckTransferRef/checkTransferRef/app.py
@@ -1,12 +1,20 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    print('Hello')
-    hi = "123"
+    body = json.loads(event['body'])
+    transfer_ref = body['transferRef']
+    message = ''
+    if len(transfer_ref) == 18 and transfer_ref.isdigit():
+        # save to database and send to Bank api
+        # send bank api
+        message = 'Sent transferRef to bank api.'
+    else:
+        message = 'Transfer Ref is invalid.'
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": message,
             # "location": ip.text.replace("\n", "")
         }),
     }

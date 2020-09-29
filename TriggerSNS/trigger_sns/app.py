a8cd5fb73bc004cfdc669e2ea07de13f4c25c1fa
kaotu <kao-tu@hotmail.co.th>
2020-09-29 23:39:19 +0700

    Get payload and add email body 🌚

--- TriggerSNS/trigger_sns/app.py
+++ TriggerSNS/trigger_sns/app.py
@@ -2,16 +2,26 @@ import boto3
 import json
 import os
 
+sns = boto3.client('sns')
 def lambda_handler(event, context):
-    sns = boto3.client('sns')
+    fail_date = event['date']
+    fail_time = event['time']
+    fail_image = event['image_from_s3']
+    
+    email_body = 'Hello, \nResult of fail to verify\n' + \
+        'Date: ' + fail_date + "\n" +\
+        'Time: ' + fail_time + "\n" +\
+        'Image: ' + fail_image + "\n" +\
+        'Best Regards, \nAthena'
+    
     response = sns.publish(
         TopicArn=os.environ['TopicArn'],    
-        Message='Check Fail!',
+        Message=email_body,
         Subject='Athena'
     )
     return {
         "statusCode": 200,
         "body": {
-            "message": "Send Email!",
+            "message": "Send Email Success!",
         },
     }

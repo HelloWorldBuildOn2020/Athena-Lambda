bf9239b5a2ce3b326e9ba20c87d546e5fdcfc4c6
kaotu <kao-tu@hotmail.co.th>
2020-10-15 12:33:05 +0700

    🎨 Format and fix wrong variable 🌚

--- TriggerSNS/trigger_sns/app.py
+++ TriggerSNS/trigger_sns/app.py
@@ -5,7 +5,7 @@ import psycopg2
 
 database = os.environ['DATABASE']
 port = os.environ['PORT']
-user = os.environ['USER_DB']
+user = os.environ['USER']
 password = os.environ['PASSWORD']
 host = os.environ['HOST']
 
@@ -25,17 +25,17 @@ email = cursor.fetchall()
 
 sns = boto3.client('sns')
 def lambda_handler(event, context):
-    fail_amount = event['amount'],
+    fail_amount = event['amount']
     fail_date = event['date']
     fail_time = event['time']
     fail_image = event['image_from_s3']
     
     email_body = 'Hello, \nResult of fail to verify\n' + \
-        'Amount: ' + fail_amount + "\n" + \
-        'Date: ' + fail_date + "\n" + \
-        'Time: ' + fail_time + "\n" + \
-        'Image: ' + fail_image + "\n" + \
-        'You can check on https://athena.khotor.live/' + "\n" + \
+        'Amount: ' + fail_amount + '\n' + \
+        'Date: ' + fail_date + '\n' + \
+        'Time: ' + fail_time + '\n' + \
+        'Image: ' + fail_image + '\n' + \
+        'You can check on https://athena.khotor.live/' + '\n' + \
         'Best Regards, \nAthena'
     
     for each_email in email:
@@ -43,7 +43,7 @@ def lambda_handler(event, context):
             TopicArn=os.environ['TopicArn'],
             Protocol='email',
             Endpoint=each_email[0],
-            ReturnSubscriptionArn=False
+            ReturnSubscriptionArn=True
         )
 
     response = sns.publish(
@@ -51,6 +51,7 @@ def lambda_handler(event, context):
         Message=email_body,
         Subject='Athena'
     )
+
     return {
         "statusCode": 200,
         "body": {
